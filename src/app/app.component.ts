01f031c0d3a23728264550319743533f783e6da9
Ivar <ivar.saris@blueelevation.nl>
2024-09-15 22:06:58 +0200

    implement routing for users and tasks components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { UsersComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent, TasksComponent],
+  imports: [RouterOutlet, UsersComponent, TasksComponent, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { UsersComponent } from './users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'tasks',
+        component: TasksComponent,
+    },
+    {
+        path: 'users',
+        component: UsersComponent,
+    }
+];
