7573506c8780a17d540813d01154e9e5fc9db0ba
Ivar <ivar.saris@blueelevation.nl>
2024-09-16 08:14:10 +0200

    set hardcoded users list

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { users_list } from './users.list';
+import { type User } from './user.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
-
+  users_list = users_list;
 }
---
+++ src/app/users/users.list.ts
@@ -0,0 +1,52 @@
+export const users_list = [
+    {
+        id: 0,
+        name: 'Unassigned',
+        avatar: 'https://as2.ftcdn.net/v2/jpg/04/62/12/13/1000_F_462121328_LoZ2Pp4CNl0zM4iXttuiaD0CpbLYbyEk.jpg'
+    },
+    {
+        id: 1,
+        name: 'Piet',
+        avatar: 'https://avatar.iran.liara.run/public/29'
+    },
+    {
+        id: 2,
+        name: 'Sarah',
+        avatar: 'https://avatar.iran.liara.run/public/30'
+    },
+    {
+        id: 3,
+        name: 'Henk',
+        avatar: 'https://avatar.iran.liara.run/public/31'
+    },
+    {
+        id: 4,
+        name: 'Kim',
+        avatar: 'https://avatar.iran.liara.run/public/32'
+    },
+    {
+        id: 5,
+        name: 'Klaas',
+        avatar: 'https://avatar.iran.liara.run/public/33'
+    },
+    {
+        id: 6,
+        name: 'Laura',
+        avatar: 'https://avatar.iran.liara.run/public/34'
+    },
+    {
+        id: 7,
+        name: 'Jaap',
+        avatar: 'https://avatar.iran.liara.run/public/35'
+    },
+    {
+        id: 8,
+        name: 'Anna',
+        avatar: 'https://avatar.iran.liara.run/public/36'
+    },
+    {
+        id: 9,
+        name: 'Guido',
+        avatar: 'https://avatar.iran.liara.run/public/37'
+    }
+]
