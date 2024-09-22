d04ddecfbb9140977660ff23c1593fe6ae66a44b
Ivar <ivar.saris@blueelevation.nl>
2024-09-22 09:43:37 +0200

    sort tasks list by status

---
+++ src/app/tasks/task.parts.list.ts
@@ -0,0 +1,2 @@
+export const task_statuses = ['To do', 'In progress', 'Under review', 'Done'];
+export const task_priorities = [1,2,3,4,5];
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,8 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { type Task } from './task.model';
 import { tasks_list } from './tasks.list';
+import { task_statuses } from './task.parts.list';
+import { task_priorities } from './task.parts.list';
 
 @Component({
   selector: 'app-tasks',
@@ -10,6 +12,14 @@ import { tasks_list } from './tasks.list';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
+
 export class TasksComponent {
   tasks_list = tasks_list;
+  task_statuses = task_statuses;
+  task_priorities = task_priorities;
+
+
+  getTasksByStatus(status: string) {
+    return this.tasks_list.filter(task => task.status === status);
+  }
 }
