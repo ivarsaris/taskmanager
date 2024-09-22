0844cd71ad352a38cff9e5c6c25f3ac7ea290dc6
Ivar <ivar.saris@blueelevation.nl>
2024-09-22 11:17:08 +0200

    make tasks filterable by assigned user

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -5,11 +5,12 @@ import { tasks_list } from './tasks.list';
 import { task_statuses } from './task.parts.list';
 import { task_priorities } from './task.parts.list';
 import { users_list } from '../users/users.list';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
@@ -19,14 +20,25 @@ export class TasksComponent {
   task_statuses = task_statuses;
   task_priorities = task_priorities;
   users_list = users_list;
-
+  selectedUserId: number = -1;
+  tasksToDisplay = this.tasks_list;
 
   getTasksByStatus(status: string) {
-    return this.tasks_list.filter(task => task.status === status);
+    return this.tasksToDisplay.filter(task => task.status === status);
   }
 
   getUserById(id: number) {
     const user = this.users_list.find(user => user.id === id);
     return user?.name;
+  }
+
+  filterTasksByUser(event: any) {
+    const id = Number(event.target.value);
+
+    if (id === -1) {
+      this.tasksToDisplay = this.tasks_list;
+    } else {
+      this.tasksToDisplay = this.tasksToDisplay.filter(task => task.assignee_id === id);
+    }
   }
 }
