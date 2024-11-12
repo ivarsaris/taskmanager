ce70a7abf4513bf6f611ddf57939febac8b80a49
Ivar <ivar.saris@blueelevation.nl>
2024-11-12 10:16:46 +0100

    create new-department component, create form

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -2,12 +2,12 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { Department } from './department.model';
 import { department_list } from './departments.list';
-
+import { NewDepartmentComponent } from './new-department/new-department.component';
 
 @Component({
   selector: 'app-departments',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NewDepartmentComponent],
   templateUrl: './departments.component.html',
   styleUrl: './departments.component.scss'
 })
---
+++ src/app/departments/new-department/new-department.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-department',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-department.component.html',
+  styleUrl: './new-department.component.scss'
+})
+export class NewDepartmentComponent {
+  createNewDepartment() {
+    
+  }
+}
